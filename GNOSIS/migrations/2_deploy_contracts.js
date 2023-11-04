abb9ed48ed34030482e8b2f32bd7b7f01be273d4
serotoninmix <109243700+serotoninmix@users.noreply.github.com>
2023-11-04 21:00:35 +0100

    Delete GNOSIS directory

--- GNOSIS/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const Survey = artifacts.require("SustainabilityQSurvey");
-
-module.exports = function(deployer) {
-  deployer.deploy(Survey);
-  // If you have other contracts, deploy them here as well
-};
